ca739d2f879e49ff72401ce38725c5ddb01a464e
kriminyurii <yator0o+github@gmail.com>
2024-12-22 23:36:17 +0200

    Refactor RadialProgressTimer for more smoothest animation

--- src/components/Common/Time/RadialProgressTimer/RadialProgressTimer.tsx
+++ src/components/Common/Time/RadialProgressTimer/RadialProgressTimer.tsx
@@ -2,88 +2,70 @@ import { memo, useCallback, useEffect, useMemo, useState } from "react";
 import clsx from "clsx";
 import RadialProgress from "../../RadialProgress/RadialProgress";
 import { getDuration } from "../../../../hooks/useTimer";
-import Timer from "../Timer";
+import Timer, { TimerProps } from "../Timer";
 import useRequestAnimationFrame from "../../../../hooks/useRequestAnimationFrame";
 import styles from "./radialprogresstimer.module.css";
 
-interface RadialProgressTimerProps {
-	children: Date;
-	className?: string;
-	paused?: boolean;
-	finished?: boolean;
-	onChange?: (duration: number | null) => void;
-	onFinish?: () => void;
-	onPause?: () => void;
-	onResume?: () => void;
-}
+const calculateValue = (startDuration: number, finishTime: number) => {
+  const currentDuration = getDuration(finishTime) || 0;
+  return startDuration > 0
+    ? Math.max(0, (startDuration - currentDuration) / startDuration)
+    : 0;
+};
 
 function RadialProgressTimer({
-	className,
-	children,
-	onFinish,
-	onPause,
-	onResume,
-	paused = false,
-}: RadialProgressTimerProps) {
-	const [finishTime, setFinishTime] = useState<number>(children.getTime());
-	const [finished, setFinished] = useState<boolean>(false);
-	const [value, setValue] = useState<number>(0);
-	const startDuration = useMemo(() => getDuration(children.getTime()) || 0, []);
+  className,
+  children,
+  isRunning = true,
+  onFinish,
+  onPause,
+  onResume,
+}: TimerProps) {
+  const [finishTime, setFinishTime] = useState<number>(children.getTime());
+  const [finished, setFinished] = useState<boolean>(false);
+  const [value, setValue] = useState<number>(0);
+  const startDuration = useMemo(() => getDuration(children.getTime()) || 0, []);
 
-	const handleNext = useCallback(() => {
-		const currentDuration = getDuration(finishTime) || 0;
+  const handleNext = useCallback(() => {
+    const value = calculateValue(startDuration, finishTime);
+    setValue(value);
+  }, [finishTime]);
 
-		let value =
-			startDuration > 0
-				? Math.max(0, (startDuration - currentDuration) / startDuration)
-				: 0;
-		setValue(value);
-	}, [finishTime]);
+  useRequestAnimationFrame(finished || !isRunning ? null : handleNext);
 
-	const { pause: pauseRaf, resume: resumeRaf } = useRequestAnimationFrame(
-		finished ? null : handleNext
-	);
+  const handleFinished = useCallback(() => {
+    setFinished(true);
+    onFinish?.();
+  }, [onFinish]);
 
-	const handleFinished = useCallback(() => {
-		setFinished(true);
-		onFinish?.();
-	}, [onFinish]);
+  const handleTimerResume = useCallback(
+    (finishTime: number) => {
+      setFinishTime(finishTime);
+      const value = calculateValue(startDuration, finishTime);
+      setValue(value);
+      onResume?.();
+    },
+    [onResume],
+  );
 
-	const handleTimerResume = useCallback(
-		(finishTime: number) => {
-			setFinishTime(finishTime);
-			onResume?.();
-		},
-		[onResume]
-	);
-
-	useEffect(() => {
-		if (paused) {
-			pauseRaf();
-			onPause?.();
-		} else {
-			resumeRaf();
-			onResume?.();
-		}
-	}, [paused, onPause, onResume]);
-
-	return (
-		<RadialProgress
-			className={clsx(styles.radialProgressTimer, className)}
-			value={value}
-			min={0}
-			max={1}
-		>
-			<Timer
-				onFinish={handleFinished}
-				onResume={handleTimerResume}
-				paused={paused}
-				className={styles.time}
-			>
-				{children}
-			</Timer>
-		</RadialProgress>
-	);
+  return (
+    <RadialProgress
+      className={clsx(styles.radialProgressTimer, className)}
+      value={value}
+      min={0}
+      max={1}
+    >
+      <Timer
+        onFinish={handleFinished}
+        onPause={onPause}
+        onResume={handleTimerResume}
+        isRunning={isRunning}
+        className={styles.time}
+      >
+        {children}
+      </Timer>
+    </RadialProgress>
+  );
 }
 
-export default memo(RadialProgressTimer);
+export default RadialProgressTimer;
