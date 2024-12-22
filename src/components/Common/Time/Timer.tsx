8179afb95602a3d0368e0d928708f9f7a84bd834
kriminyurii <yator0o+github@gmail.com>
2024-12-22 22:16:02 +0200

    Refactor Timer to use declarative isRunning

--- src/components/Common/Time/Timer.tsx
+++ src/components/Common/Time/Timer.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, memo } from "react";
 import useTimer from "../../../hooks/useTimer";
 
 const SECOND = 1000;
@@ -6,55 +6,53 @@ const SEC_IN_MINUTE = 60;
 const MINS_IN_HOUR = 60;
 
 interface TimerProps {
-	className?: string;
-	children: Date;
-	paused?: boolean;
-	onChange?: (duration: number | null) => void;
-	onFinish?: () => void;
-	onPause?: () => void;
-	onResume?: (finishTime: number) => void;
+  className?: string;
+  children: Date;
+  isRunning?: boolean;
+  onChange?: (duration: number | null) => void;
+  onFinish?: () => void;
+  onPause?: () => void;
+  onResume?: (finishTime: number) => void;
 }
 
 const formatTime = (duration: number | null) => {
-	if (duration === null) return "00:00";
+  if (duration === null) return "00:00";
 
-	const durationSeconds = Math.ceil(duration / SECOND);
-	let seconds = durationSeconds % SEC_IN_MINUTE;
-	let minutes = Math.floor(durationSeconds / SEC_IN_MINUTE) % MINS_IN_HOUR;
+  const durationSeconds = Math.ceil(duration / SECOND);
+  let seconds = durationSeconds % SEC_IN_MINUTE;
+  let minutes = Math.floor(durationSeconds / SEC_IN_MINUTE) % MINS_IN_HOUR;
 
-	return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
-		2,
-		"0"
-	)}`;
+  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+    2,
+    "0",
+  )}`;
 };
 
 const Timer = ({
-	className,
-	children,
-	paused = false,
-	onFinish,
-	onPause,
-	onResume,
+  className,
+  children,
+  isRunning = false,
+  onFinish,
+  onPause,
+  onResume,
 }: TimerProps) => {
-	const { duration, finished, finishTime, pause, resume } = useTimer(children);
+  const { duration, finished, finishTime } = useTimer(children, isRunning);
 
-	useEffect(() => {
-		if (finished && onFinish) {
-			onFinish();
-		}
-	}, [finished, onFinish]);
+  useEffect(() => {
+    if (finished && onFinish) {
+      onFinish();
+    }
+  }, [finished, onFinish]);
 
-	useEffect(() => {
-		if (paused) {
-			pause();
-			onPause?.();
-		} else {
-			resume();
-			onResume?.(finishTime);
-		}
-	}, [paused, pause, resume, onPause, onResume]);
+  useEffect(() => {
+    if (isRunning) {
+      onResume?.(finishTime);
+    } else {
+      onPause?.();
+    }
+  }, [onPause, isRunning, onResume, finishTime]);
 
-	return <time className={className}>{formatTime(duration)}</time>;
+  return <time className={className}>{formatTime(duration)}</time>;
 };
 
-export default Timer;
+export default memo(Timer);
