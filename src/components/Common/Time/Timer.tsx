68e9c9aba0410779b28d6e4c3c27a49d247d585d
kriminyurii <yator0o+github@gmail.com>
2024-12-26 18:20:13 +0200

    Refactor timer with test buttons

--- src/components/Common/Time/Timer.tsx
+++ src/components/Common/Time/Timer.tsx
@@ -1,57 +1,60 @@
-import { useEffect, memo } from "react";
+import { memo, useCallback, useState } from "react";
 import useTimer from "../../../hooks/useTimer";
+import clsx from "clsx";
+import styles from "./timer.module.css";
+import Button from "../Button/Button";
 
 const SECOND = 1000;
 const SEC_IN_MINUTE = 60;
 const MINS_IN_HOUR = 60;
 
 export interface TimerProps {
-  className?: string;
-  children: Date;
-  isRunning?: boolean;
-  onFinish?: () => void;
-  onPause?: () => void;
-  onResume?: (finishTime: number) => void;
+	className?: string;
+	children: number;
+	isRunning?: boolean;
+	onFinish?: () => void;
+	onPause?: () => void;
+	onResume?: (finishTime: number) => void;
 }
 
 const formatTime = (duration: number | null) => {
-  if (duration === null) return "00:00";
+	if (duration === null) return "00:00";
 
-  const durationSeconds = Math.ceil(duration / SECOND);
-  let seconds = durationSeconds % SEC_IN_MINUTE;
-  let minutes = Math.floor(durationSeconds / SEC_IN_MINUTE) % MINS_IN_HOUR;
+	const durationSeconds = Math.ceil(duration / SECOND);
+	let seconds = durationSeconds % SEC_IN_MINUTE;
+	let minutes = Math.floor(durationSeconds / SEC_IN_MINUTE) % MINS_IN_HOUR;
 
-  return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
-    2,
-    "0",
-  )}`;
+	return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+		2,
+		"0"
+	)}`;
 };
 
-const Timer = ({
-  className,
-  children,
-  isRunning = false,
-  onFinish,
-  onPause,
-  onResume,
-}: TimerProps) => {
-  const { duration, finished, finishTime } = useTimer(children, isRunning);
+const getCurrentFinishDate = (duration: number) =>
+	new Date(Date.now() + duration);
 
-  useEffect(() => {
-    if (finished && onFinish) {
-      onFinish();
-    }
-  }, [finished, onFinish]);
+const Timer = ({ className, children, onFinish }: TimerProps) => {
+	const [finishDate, setFinishDate] = useState(getCurrentFinishDate(children));
+	const { duration, start, pause, resume } = useTimer(finishDate, onFinish);
 
-  useEffect(() => {
-    if (isRunning) {
-      onResume?.(finishTime);
-    } else {
-      onPause?.();
-    }
-  }, [onPause, isRunning, onResume, finishTime]);
+	const handleStart = useCallback(() => {
+		const finishDate = getCurrentFinishDate(children);
+		setFinishDate(finishDate);
+		start(finishDate);
+	}, [children, start]);
 
-  return <time className={className}>{formatTime(duration)}</time>;
+	const handleResume = useCallback(() => {
+		resume(finishDate);
+	}, [resume]);
+
+	return (
+		<div className={clsx(styles.timer, className)}>
+			<time className={styles.time}>{formatTime(duration)}</time>
+			<Button onClick={handleStart}>Start</Button>
+			<Button onClick={pause}>Pause</Button>
+			<Button onClick={handleResume}>Resume</Button>
+		</div>
+	);
 };
 
 export default memo(Timer);
