27811f00180cf41ab5eb704ce41e5d14b931b4fa
kriminyurii <yator0o+github@gmail.com>
2024-12-27 14:41:39 +0200

    Add PomodoroStep

---
+++ src/components/PomodoroCycleProgress/PomodoroStep/PomodoroStep.tsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import clsx from "clsx";
+import { usePomodoro } from "../PomodoroStepsHost";
+import styles from "./pomodorostep.module.css";
+
+interface PomodoroStepProps {
+	children: string;
+	className?: string;
+}
+
+const PomodoroStep = ({ children, className }: PomodoroStepProps) => {
+	const { currentStep } = usePomodoro();
+
+	return (
+		<div
+			className={clsx(styles.item, className, {
+				[styles.active]: children === currentStep,
+			})}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default memo(PomodoroStep);
