5cb36d6aabc5020b32baadbb274480470e62365d
kriminyurii <yator0o+github@gmail.com>
2024-12-28 10:31:28 +0200

    Add step and timer to the MainWidget

--- src/components/MainWiget/MainWiget.tsx
+++ src/components/MainWiget/MainWiget.tsx
@@ -1,31 +1,54 @@
 import { useState } from "react";
-import Timer from "../Common/Time/Timer";
+import PomodoroCycleProgress, {
+	LONG_BREAK,
+	POMODORO,
+	SHORT_BREAK,
+} from "../PomodoroCycleProgress/PomodoroCycleProgress";
+import PomodoroTimer from "../PomodoroTimer/PomodoroTimer";
 import styles from "./mainwidget.module.css";
 
-const twentyFiveMinutesFromNow = new Date(Date.now() + 30000);
+const SECOND = 1000;
+const SEC_IN_MINUTE = 60;
+
+const POMODORO_TIMER = 25 * SEC_IN_MINUTE * SECOND;
+const SHORT_BREAK_TIMER = 5 * SEC_IN_MINUTE * SECOND;
+const LONG_BREAK_TIMER = 30 * SEC_IN_MINUTE * SECOND;
 
 export default function MainWidget() {
-	const [paused, setPaused] = useState<boolean>(false);
+	const [currentPomodoroStep, setCurrentPomodoroStep] = useState(POMODORO);
+	const [timerDuration, setTimerDuration] = useState(POMODORO_TIMER);
 
-	const pauseTimer = () => {
-		setPaused(true);
-	};
-
-	const resumeTimer = () => {
-		setPaused(false);
+	const handleFinish = () => {
+		switch (currentPomodoroStep) {
+			case POMODORO:
+				setTimerDuration(SHORT_BREAK_TIMER);
+				setCurrentPomodoroStep(SHORT_BREAK);
+				break;
+			case "Short Break":
+				setTimerDuration(LONG_BREAK_TIMER);
+				setCurrentPomodoroStep(LONG_BREAK);
+				break;
+			case "Long Break":
+			default:
+				setTimerDuration(POMODORO_TIMER);
+				setCurrentPomodoroStep(POMODORO);
+				break;
+		}
 	};
 
 	return (
 		<div className={styles.mainWidget}>
-			<Timer paused={paused} className={styles.timer}>
-				{twentyFiveMinutesFromNow}
-			</Timer>
-			<button type="button" onClick={pauseTimer}>
-				Pause
-			</button>
-			<button type="button" onClick={resumeTimer}>
-				Resume
-			</button>
+			<PomodoroCycleProgress
+				currentStep={currentPomodoroStep}
+				className={styles.progress}
+			/>
+			<PomodoroTimer
+				key={timerDuration}
+				onFinish={handleFinish}
+				className={styles.timer}
+			>
+				{timerDuration}
+			</PomodoroTimer>
 		</div>
 	);
 }
