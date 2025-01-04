d9ab6ba14eb3bd10741347bce2e2829bf74081f3
kriminyurii <yator0o+github@gmail.com>
2025-01-04 15:46:50 +0200

    Refactor Item to work with new type

--- src/components/Common/ListView/Item.tsx
+++ src/components/Common/ListView/Item.tsx
@@ -1,29 +1,26 @@
 import { ReactNode, memo } from "react";
 import clsx from "clsx";
-import { useListView } from "./ListView";
+import { Task, useListView } from "./ListViewContextHost";
 import styles from "./item.module.css";
 
 interface ItemProps {
-	id: React.Key;
+	task: Task;
 	children: ReactNode;
 	className?: string;
 }
 
-const Item = ({ id, children, className }: ItemProps) => {
-	const context = useListView();
-
-	const { state, onAction } = context;
-	const { selectedKeys, toggleKey } = state;
+const Item = ({ task, children, className }: ItemProps) => {
+	const { toggleTask, onAction } = useListView();
 
 	const handleClick = () => {
-		toggleKey(id);
-		onAction?.(id);
+		toggleTask(task);
+		onAction?.(task);
 	};
 
 	return (
 		<div
 			className={clsx(styles.item, className, {
-				[styles.active]: selectedKeys.has(id),
+				[styles.active]: task.completed,
 			})}
 			onClick={handleClick}
 		>
