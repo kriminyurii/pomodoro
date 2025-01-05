cbb7bf05526564d76a068c0d09d2991cc5aff281
kriminyurii <yator0o+github@gmail.com>
2025-01-05 14:46:48 +0200

    Remove unused code

--- src/hooks/useRequestAnimationFrame.ts
+++ src/hooks/useRequestAnimationFrame.ts
@@ -1,28 +1,27 @@
-import { useEffect, useRef, useCallback } from "react";
+import { useEffect, useRef } from "react";
 
 const useRequestAnimationFrame = (
-  onNext: ((perfNow: number) => void) | null,
-  onStop?: (perfNow: number) => void,
+	onNext: ((perfNow: number) => void) | null,
+	onStop?: (perfNow: number) => void
 ) => {
-  const requestRef = useRef<number | null>(null);
-  const isPaused = useRef(false);
+	const requestRef = useRef<number | null>(null);
 
-  const handleNext = () => {
-    onNext(performance.now());
-    requestRef.current = requestAnimationFrame(handleNext);
-  };
+	const handleNext = () => {
+		onNext?.(performance.now());
+		requestRef.current = requestAnimationFrame(handleNext);
+	};
 
-  useEffect(() => {
-    if (onNext) {
-      requestRef.current = requestAnimationFrame(handleNext);
-    }
-    return () => {
-      if (requestRef.current !== null) {
-        cancelAnimationFrame(requestRef.current);
-        onStop?.(performance.now());
-      }
-    };
-  }, [onNext, onStop]);
+	useEffect(() => {
+		if (onNext) {
+			requestRef.current = requestAnimationFrame(handleNext);
+		}
+		return () => {
+			if (requestRef.current !== null) {
+				cancelAnimationFrame(requestRef.current);
+				onStop?.(performance.now());
+			}
+		};
+	}, [onNext, onStop]);
 };
 
 export default useRequestAnimationFrame;
